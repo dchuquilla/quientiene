2915a63c440920e6342a684f51e4535cda9bc0e8
dchuquilla <dario.chuquilla@gmail.com>
2021-08-26 12:52:10 -0500

    fix image purge on updates Best way for showing imgaes

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,7 +4,8 @@ class Shop < ApplicationRecord
   has_many :replacement_proposals
 
   validates :name, :address, :ruc, :phone1, presence: true
-  validates :ruc, format: { with: /\A[0-9]+\z/, message: "solo admite números" }
-  validates :phone1, format: { with: /\A[0-9]+\z/, message: "solo admite números" }
+  validates :ruc, format: { with: /[0-9]+/, message: "solo admite números" }
+  validates :phone1, format: { with: /09([0-9]{8})+/, message: "no es correcto " }
+  validates :phone2, format: { with: /[0-9]+/, message: "solo admite números" }
   validates :ruc, uniqueness: true
 end
