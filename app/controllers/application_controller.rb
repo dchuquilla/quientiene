f768d12b955e1db5fca05d07b41cbd0b480c7726
dchuquilla <dario.chuquilla@gmail.com>
2021-09-23 18:02:46 -0500

    Favicon y metadata

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   add_flash_types :info, :error, :warning
 
+  before_action :set_metadata
+
   # The path used after sign up.
   def after_sign_in_path_for(resource)
     dashboard_path
+  end
+
+  def set_metadata
+    @title = "QuienTiene, encuentre el repuesto que está buscando"
   end
 end
