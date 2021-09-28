751a5cbb148ee9a58877fe69eb7621a36ce14a45
dchuquilla <dario.chuquilla@gmail.com>
2021-09-28 12:37:41 -0500

    Skip confirmation registration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable,
+         :lockable,
          :omniauthable, omniauth_providers: [:facebook, :google_oauth2, :twitter]
 
   has_many :vehicles, dependent: :destroy
